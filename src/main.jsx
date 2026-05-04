a586b1912f66204c7ed76b3fae6ae59d410a3e66
HanishS9074 <nagar9074@gmail.com>
2026-05-04 15:12:39 +0530

    fix: base path for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes.jsx";
-import PortfolioProvider from "./context/portfolioContext";
+import PortfolioProvider from "./context/portfolioContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
